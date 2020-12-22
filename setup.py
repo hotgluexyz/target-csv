c5ac655cedcd248d4e127a81f835f78b77effecc
Irfan Zulfiqar <irfan@buckitapp.co>
2020-12-21 22:06:01 -0500

    fixed version range for singer-python.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='target-csv',
       py_modules=['target_csv'],
       install_requires=[
           'jsonschema==2.6.0',
-          'singer-python==5.3.1',
+          'singer-python>=5.1.0,<=5.3.1',
       ],
       entry_points='''
           [console_scripts]
