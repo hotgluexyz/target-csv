b6b0ecc5bddd80df081319f727670e86ebafd8f9
LiorB-D <liorbd@outlook.com>
2026-08-24 17:36:36 -0400

    Simplify pr

--- target_csv.py
+++ target_csv.py
@@ -7,17 +7,10 @@ import sys
 import json
 import simplejson
 import csv
-import threading
-import http.client
-import urllib
 from datetime import datetime
 import collections
 import pathlib
 
-try:
-    import pkg_resources
-except ModuleNotFoundError:  # Python 3.14 no longer bundles pkg_resources.
-    pkg_resources = None
 
 from jsonschema.validators import Draft4Validator
 import singer
@@ -145,29 +138,6 @@ def persist_messages(delimiter, quotechar, messages, destination_path, fixed_hea
     return state
 
 
-def send_usage_stats():
-    try:
-        if pkg_resources is None:
-            from importlib.metadata import version as distribution_version
-            version = distribution_version('target-csv')
-        else:
-            version = pkg_resources.get_distribution('target-csv').version
-        conn = http.client.HTTPConnection('collector.singer.io', timeout=10)
-        conn.connect()
-        params = {
-            'e': 'se',
-            'aid': 'singer',
-            'se_ca': 'target-csv',
-            'se_ac': 'open',
-            'se_la': version,
-        }
-        conn.request('GET', '/i?' + urllib.parse.urlencode(params))
-        response = conn.getresponse()
-        conn.close()
-    except:
-        logger.debug('Collection request failed')
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', help='Config file')
@@ -178,12 +148,6 @@ def main():
             config = json.load(input_json)
     else:
         config = {}
-
-    if not config.get('disable_collection', False):
-        logger.info('Sending version information to singer.io. ' +
-                    'To disable sending anonymous usage data, set ' +
-                    'the config parameter "disable_collection" to true')
-        threading.Thread(target=send_usage_stats).start()
 
     input_messages = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
     state = persist_messages(config.get('delimiter', ','),
